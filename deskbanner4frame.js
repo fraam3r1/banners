e8052977a1f27d875877a914328b3c2e07f9fcb0
fraam3r1 <107279491+fraam3r1@users.noreply.github.com>
2022-09-02 16:32:11 +0200

    Add files via upload

--- deskbanner4frame.js
+++ deskbanner4frame.js
@@ -6,7 +6,6 @@ const SVGComponent = (props) => (
     xmlns="http://www.w3.org/2000/svg"
     xmlnsXlink="http://www.w3.org/1999/xlink"
     viewBox="0 0 1500 500"
-    {...props}
   >
     <defs>
       <clipPath id="clippath">
